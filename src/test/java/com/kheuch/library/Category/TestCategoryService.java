1cab72f49bfc42a7c2a9e5038ca7109cbdab1412
Cheikhou Seck <kheus1990@gmail.com>
2019-12-13 16:13:23 +0100

    ajout test pour CategoryService

---
+++ src/test/java/com/kheuch/library/Category/TestCategoryService.java
@@ -0,0 +1,11 @@
+package com.kheuch.library.Category;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestCategoryService {
+
+    public void saveCategoryTest(String category){
+
+    }
+}
